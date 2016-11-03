d6ddcd377881f704b15c634495cf17065f18e8a7
JASON CAIN <cain.ja@husky.neu.edu>
2016-11-03 12:25:56 -0400

    Made some changes to laminar flow, added some tryptophan information

--- laminarFlow.py
+++ laminarFlow.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 class LaminarFlow:
-    def __init__(self, n, serConcentration, serGraetz, effPermeability, M_Beta_file = 'valuesBetaM.txt'):
+    def __init__(self, n, serConcentration, serGraetz, effSerPermeability, M_Beta_file = 'valuesBetaM.txt'):
         '''
         Initializes the variables to run begin running the simulation: Maybe we make this general so we can use the same class
         to run Serotonin and Tryptophan. Haven't really decided yet. Different compounds would change our Graetz number as well.
         Beta and M values come from the mathematical solution to the differential equations.
         '''
-        self.graetz = serGraetz
+        self.serGraetz = serGraetz
+        # self.trypGraetz = trypGraetz
         self.serConcentration = np.zeros(n)
         self.serConcentration[0] = serConcentration
         # self.trypConcentration = np.zeros(n)
@@ -19,13 +20,15 @@ class LaminarFlow:
         self.calculateConcentration()
 
     def calculateConcentration(self):
-        MBetaList = interpolateForValue(self.effPermeability, self.MBeta)
-        for i in range(len(serConcentration)):
+        serMBetaList = interpolateForValue(self.effSerPermeability, self.MBeta)
+        # trypMBetaList = interpolateForValue(self.effTrypPermeability, self.MBeta)
+        for i in range(len(serConcentration)-1):
             Delta = 0
             for j in range(5):
-                Delta += MBetaList[j+5]*np.exp(MBetaList[j]**2*self.graetz) #Beta * 5 first in file, then M * 5
-
-            serConcentration[i] = serConcentration[i-1]*Delta
+                serDelta += serMBetaList[j+5]*np.exp(serMBetaList[j]**2*self.serGraetz) #Beta * 5 first in file, then M * 5
+                # trypDelta += trypMBetaList[j+5]*np.exp(trypMBetaList[j]**2*self.trypGraetz)
+            serConcentration[i+1] = serConcentration[i]*serDelta
+            trypConcentration[i+1] = trypConcentration[i]*serDelta
 
 def interpolateForValue(value, array):
     fromarray = array[0,:]
