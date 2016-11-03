c9c6b01c8b2f18d4d7cb87944f2eac8ccb9057a6
JASON CAIN <cain.ja@husky.neu.edu>
2016-11-03 10:40:52 -0400

    Added solver for differential equation

--- laminarFlow.py
+++ laminarFlow.py
@@ -1,12 +1,31 @@
+import numpy as np
+
 class LaminarFlow:
-    def __init__(self, serConcentration, trypConcentration, graetz, effPermeability):
-        self.graetz = graetz
-        self.serConcentration = serConcentration
-        self.trypConcentration = trypConcentration
+    def __init__(self, n, serConcentration, serGraetz, effPermeability, M_Beta_file = 'valuesBetaM.txt'):
+        '''
+        Initializes the variables to run begin running the simulation: Maybe we make this general so we can use the same class
+        to run Serotonin and Tryptophan. Haven't really decided yet. Different compounds would change our Graetz number as well.
+        Beta and M values come from the mathematical solution to the differential equations.
+        '''
+        self.graetz = serGraetz
+        self.serConcentration = np.zeros(n)
+        self.serConcentration[0] = serConcentration
+        # self.trypConcentration = np.zeros(n)
+        # self.trypConcentration[0] = trypConcentration
         self.effPermeability = effpermeability
+        with open(M_Beta_file, 'r') as myfile:
+            self.MBeta = np.fromfile(myfile)
+
+        self.calculateConcentration()
 
     def calculateConcentration(self):
-        #insert differential equations
+        MBetaList = interpolateForValue(self.effPermeability, self.MBeta)
+        for i in range(len(serConcentration)):
+            Delta = 0
+            for j in range(5):
+                Delta += MBetaList[j+5]*np.exp(MBetaList[j]**2*self.graetz) #Beta * 5 first in file, then M * 5
+
+            serConcentration[i] = serConcentration[i-1]*Delta
 
 def interpolateForValue(value, array):
     fromarray = array[0,:]
