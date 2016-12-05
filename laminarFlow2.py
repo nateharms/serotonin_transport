1e7a825fc08030d6715fd7d93d0f6081d828e58c
JASON CAIN <cain.ja@husky.neu.edu>
2016-12-05 11:02:00 -0500

    Edited laminarFlow for namedtuples

--- laminarFlow2.py
+++ laminarFlow2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class LaminarFlow:
-    def __init__(self, length, radius, serConcentration, trypConcentration, n, M_Beta_file = 'valuesBetaM.csv'):
+    def __init__(self, length, radius, serConditions, trypConditions, n, M_Beta_file = 'valuesBetaM.csv'):
         """
         Initializes the variables to run begin running the simulation: Maybe we make this general so we can use the same class
         to run Serotonin and Tryptophan. Haven't really decided yet. Different compounds would change our Graetz number as well.
@@ -11,8 +11,8 @@ class LaminarFlow:
         ------------------------------------------------------------------------------------
         length: the length of the intestines (m)
         radius: the radius of the intestines (m)
-        serConcentration: the initial concentration of seritonin in the intestines (mol/L)
-        trypConcentration: the initial concentration of tryp in the intestines (mol/L)
+        serConditions: type: NamedTuple, with ('Concentration', 'Diffusivity', 'Wall Permeability')
+        trypConcentration: type: NamedTuple, with ('Concentration', 'Diffusivity', 'Wall Permeability')
         n: the number of sub-intervals of intestines looked at (unitless)
         M_Beta_file: the file that contains the M_Beta values needed for calculations
         """
@@ -23,18 +23,18 @@ class LaminarFlow:
 
         # The initial concentrations for both of the tryp an ser.
         self.serConcentration = np.zeros(n)
-        self.serConcentration[0] = serConcentration
+        self.serConcentration[0] = serConditions.Concentration
 
         self.trypConcentration = np.zeros(n)
-        self.trypConcentration[0] = trypConcentration
+        self.trypConcentration[0] = trypConditions.Concentration
 
         # The diffusivities, wall permabilities, and effective permabilities for ser and tryp.
-        self.serDiffusivity = 6.2424e-8 # m^2/sec
-        self.serWallPerm = 7.576e-13 # m^2/sec ...?
+        self.serDiffusivity = serConditions.Diffusivity #6.2424e-8 m^2/sec
+        self.serWallPerm = serConditions.Permeability #7.576e-13 m^2/sec
         self.serEffPerm = self.serWallPerm * self.radius / self.serDiffusivity
 
-        self.trypDiffusivity = 5.386e-8 # m^2/sec
-        self.trypWallPerm = 6.44e-4 # m^2/sec
+        self.trypDiffusivity = trypConditions.Diffusivity #5.386e-8 m^2/sec
+        self.trypWallPerm = trypConditions.Permeability #6.44e-4 m^2/sec
         self.trypEffPerm = self.trypWallPerm * self.radius / self.trypDiffusivity
 
         # The MBeta values
@@ -58,22 +58,11 @@ class LaminarFlow:
         serGraetz: a list of Graetz numbers for ser (length n)
         trypGraetz: a list of Graetz numbers for tryp (length n)
         """
-        lengths = []
-        serGraetz = []
-        trypGraetz = []
+
         max_velocity = 5
-        for i in range(n):
-            lengths.append(self.length * i / n)
-            serGraetz.append(self.serDiffusivity*(lengths[i]/(max_velocity*self.radius**2)))
-            trypGraetz.append(serGraetz[i]*self.trypDiffusivity/self.serDiffusivity)
-
-
-        self.lengths = np.array(lengths)
-
-        # The graetz numbers for both serotinin and trypophan.
-        self.serGraetz = np.array(serGraetz)
-        self.trypGraetz = np.array(trypGraetz)
-
+        self.lengths = lengths = np.linspace(0,self.length,n)
+        self.serGraetz = self.serDiffusivity/(max_velocity*self.radius**2)*lengths
+        self.trypGraetz = self.serGraetz*self.trypDiffusivity/self.serDiffusivity
 
     def getConcentration(self):
         """
